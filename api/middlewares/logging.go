4aa90a4261d154cb645eb7f82663fa97707d1bea
Benjamin <8467845+import-benjamin@users.noreply.github.com>
2025-04-06 15:41:59 +0200

    refactor: improve logging driver

--- api/middlewares/logging.go
+++ api/middlewares/logging.go
@@ -1,31 +1,30 @@
 package middlewares
 
 import (
+	"context"
+	"log/slog"
 	"net/http"
-
-	"github.com/charmbracelet/log"
 )
 
-type loggingResponseWriter struct {
+type statusResponseWriter struct {
 	http.ResponseWriter
 	statusCode int
 }
 
-func NewLoggingResponseWriter(w http.ResponseWriter) *loggingResponseWriter {
-	return &loggingResponseWriter{w, http.StatusOK}
-}
-
-func (lrw *loggingResponseWriter) WriteHeader(code int) {
-	lrw.statusCode = code
-	lrw.ResponseWriter.WriteHeader(code)
-}
-
-func ServerLogHandler(h http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-		lrw := NewLoggingResponseWriter(w)
-		h.ServeHTTP(w, r)
-		statusCode := lrw.statusCode
-		log.Info(r.Method, "status", statusCode, "uri", r.URL.String())
-	}
-	return http.HandlerFunc(fn)
+func LoggingHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		statusResponseWriter := &statusResponseWriter{
+			w,
+			http.StatusOK,
+		}
+		h.ServeHTTP(statusResponseWriter, r)
+		slog.LogAttrs(
+			context.Background(),
+			slog.LevelInfo,
+			"Responded to request",
+			slog.Int("status", statusResponseWriter.statusCode),
+			slog.String("method", r.Method),
+			slog.String("url", r.URL.Path),
+		)
+	})
 }
